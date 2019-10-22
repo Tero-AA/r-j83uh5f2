a457d7e0ab559241549f479ea88c61a7b4e0e92f
Andrés Ayala <53452577+Tero-AA@users.noreply.github.com>
2019-10-22 11:21:58 -0500

    added functionality to the textarea counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.updateCounter = this.updateCounter.bind(this)
+
+    this.state = {
+      counter: 0
+    }
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea
+          rows="3"
+          onChange={this.updateCounter}
+        >
+
+        </textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
+  }
+
+  updateCounter(event) {
+    this.setState({
+      counter: event.target.value.length
+    })
   }
 }
 
